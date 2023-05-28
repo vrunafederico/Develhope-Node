fec17d973331dbd182ab83aad4f68e0841f0c3f8
federico vruna <federix5@hotmail.com>
2023-05-28 11:11:17 +0200

    CRUD

--- Server.js
+++ Server.js
@@ -1,11 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Joi = require('joi');
 var express = require('express');
 var app = express();
 require('dotenv').config();
 app.use(express.json());
-app.use(function (req, res, next) {
-    console.log(req);
-    next();
-});
 var planets = [
     {
         id: 1,
@@ -16,6 +15,51 @@ var planets = [
         name: "Mars",
     },
 ];
+app.get("/api/planets", function (req, res) {
+    res.status(200).json(planets);
+});
+app.get("/api/planets/:id", function (req, res) {
+    var element = planets.find(function (el) { return el.id === Number(req.params.id); });
+    res.status(200).json(element);
+});
+app.post("/api/planets", function (req, res) {
+    var schema = Joi.object().keys({
+        id: Joi.number().integer().required(),
+        name: Joi.string().required()
+    });
+    var data = schema.validate(req.body);
+    if (data.error) {
+        res.status(404).json({ msg: "error" });
+        return;
+    }
+    planets.push(data.value);
+    res.status(200).json({ msg: "Done" });
+});
+app.put("/api/planets/:id", function (req, res) {
+    var schema = Joi.object().keys({
+        name: Joi.string().required()
+    });
+    var data = schema.validate(req.body);
+    if (data.error) {
+        res.status(404).json({ msg: "error" });
+    }
+    var object = planets.find(function (el) { return el.id === Number(req.params.id); });
+    if (object) {
+        object.name = data.value.name;
+        res.status(200).json({ msg: "Done" });
+        return;
+    }
+    res.status(404).json({ msg: "Not found" });
+});
+app.delete("/api/planets/:id", function (req, res) {
+    var index = planets.findIndex(function (el) { return el.id === Number(req.params.id); });
+    if (index === -1) {
+        res.status(404).json({ msg: "Not found" });
+        return;
+    }
+    planets.splice(index, 1);
+    res.status(200).json({ msg: "Done" });
+});
 app.listen(process.env.port, function () {
     console.log("Example app listening on port ".concat(process.env.port));
 });
--- Server.ts
+++ Server.ts
@@ -1,13 +1,11 @@
+import { array } from "joi";
+
+const Joi = require('joi')
 const express = require('express')
 const app = express()
 require('dotenv').config()
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log(req)
-  next()
-})
 
 type Planet = {
   id: number,
@@ -26,6 +24,72 @@ let planets: Planets = [
     name: "Mars",
   },
 ];
+
+
+app.get("/api/planets", (req, res) => {
+  res.status(200).json(planets)
+})
+
+app.get("/api/planets/:id",  (req, res) =>{
+  const element = planets.find(el => el.id === Number(req.params.id))
+  if(!element){
+    res.status( 404 ).json("Not found")
+    return
+  }
+  res.status( 200 ).json(element)
+})
+
+app.post("/api/planets", (req, res) =>{
+  const schema = Joi.object().keys({
+    id: Joi.number().integer().required(),
+    name: Joi.string().required()
+  })
+
+  const data = schema.validate(req.body)
+
+  if(data.error){
+    res.status(404).json({msg: "error"})
+    return
+  }
+
+  planets.push(data.value)
+  res.status(200).json({msg: "Done"})
+})
+
+app.put("/api/planets/:id", (req, res) =>{
+  const schema = Joi.object().keys({
+    name: Joi.string().required()
+  })
+
+  const data = schema.validate(req.body)
+
+  if(data.error){
+    res.status(404).json({msg: "error"})
+  }
+
+  let object = planets.find(el => el.id === Number(req.params.id))
+
+  if(object){
+    object.name = data.value.name
+    res.status(200).json({msg: "Done"})
+    return
+  }
+
+  res.status(404).json({msg: "Not found"})
+})
+
+app.delete("/api/planets/:id", (req, res) =>{
+  const index = planets.findIndex((el) => el.id === Number(req.params.id))
+
+  if(index === -1){  
+    res.status(404).json({msg: "Not found"})
+    return
+  }
+
+  planets.splice(index, 1); 
+  res.status(200).json({msg: "Done"})
+ 
+})
 
 app.listen(process.env.port, () => {
   console.log(`Example app listening on port ${process.env.port}`)
